41efbc8a11e6ff991d4c29d1212b98bc07400d89
artemka <artem.vas333@gmail.com>
2025-05-10 03:15:52 +0300

    add docker-files

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # kernel
+
+```bash
+docker-compose build osdev-x86
+docker-compose run --rm osdev-x86
+```
+
+```bash
+docker-compose build osdev-arm64
+docker-compose run --rm osdev-arm64
+```
--- arch/x86/kernel/kernel.c
+++ arch/x86/kernel/kernel.c
@@ -1,17 +1,11 @@
 #include "cli.h"
 
-
-
-
-
-
-
-void kernel_main(void) 
+void kernel_main(void)
 {
 	/* Initialize terminal interface */
 	terminal_initialize();
 
 	/* Newline support is left as an exercise. */
-	
+
 	cli();
 }
