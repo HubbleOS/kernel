fdf6aae9b6c58c852129e2d837b02f3deb267ef6
artemka <artem.vas333@gmail.com>
2025-05-23 09:41:24 +0300

    fix qemu loader script

--- scripts/qemu.sh
+++ scripts/qemu.sh
@@ -7,12 +7,14 @@
 
 # ISO_DIR=$1
 
+WORKDIR="$(pwd)"
+
 qemu-system-x86_64 \
 	-M pc \
 	-cpu Haswell \
 	-smp 2 \
 	-m 1024 \
-	-drive if=pflash,format=raw,readonly=on,file=ovmf/OVMF_CODE.fd \
-	-drive if=pflash,format=raw,file=ovmf/OVMF_VARS.fd \
+	-drive if=pflash,format=raw,readonly=on,file=$WORKDIR/ovmf/OVMF_CODE.fd \
+	-drive if=pflash,format=raw,file=$WORKDIR/ovmf/OVMF_VARS.fd \
 	-hda fat:rw:iso/x86 \
 	-serial stdio
