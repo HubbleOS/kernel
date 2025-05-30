d532d8a26535562aa45239b675952cf725f60422
artemka <artem.vas333@gmail.com>
2025-05-30 09:31:30 +0300

    add the new headers

--- arch/x86/include/syscall.h
+++ arch/x86/include/syscall.h
@@ -1,8 +1,76 @@
-#pragma once
+#ifndef _SYSCALL_H_
+#define _SYSCALL_H_
 
-#define SYS_WRITE 1
-#define SYS_READ 2
+#ifdef __cplusplus
+extern "C"
+{
+#endif
 
-typedef int (*syscall_t)(int, int, int);
+#include "syscall_numbers.h"
+	long syscall_dispatcher(long n, long a1, long a2, long a3, long a4, long a5, long a6);
 
-int syscall_dispatch(int number, int arg0, int arg1, int arg2);
+	static __inline long __syscall0(long n)
+	{
+		unsigned long ret;
+		__asm__ __volatile__("syscall" : "=a"(ret) : "a"(n) : "rcx", "r11", "memory");
+		return ret;
+	}
+
+	static __inline long __syscall1(long n, long a1)
+	{
+		unsigned long ret;
+		__asm__ __volatile__("syscall" : "=a"(ret) : "a"(n), "D"(a1) : "rcx", "r11", "memory");
+		return ret;
+	}
+
+	static __inline long __syscall2(long n, long a1, long a2)
+	{
+		unsigned long ret;
+		__asm__ __volatile__("syscall" : "=a"(ret) : "a"(n), "D"(a1), "S"(a2)
+							 : "rcx", "r11", "memory");
+		return ret;
+	}
+
+	static __inline long __syscall3(long n, long a1, long a2, long a3)
+	{
+		unsigned long ret;
+		__asm__ __volatile__("syscall" : "=a"(ret) : "a"(n), "D"(a1), "S"(a2),
+													 "d"(a3) : "rcx", "r11", "memory");
+		return ret;
+	}
+
+	static __inline long __syscall4(long n, long a1, long a2, long a3, long a4)
+	{
+		unsigned long ret;
+		register long r10 __asm__("r10") = a4;
+		__asm__ __volatile__("syscall" : "=a"(ret) : "a"(n), "D"(a1), "S"(a2),
+													 "d"(a3), "r"(r10) : "rcx", "r11", "memory");
+		return ret;
+	}
+
+	static __inline long __syscall5(long n, long a1, long a2, long a3, long a4, long a5)
+	{
+		unsigned long ret;
+		register long r10 __asm__("r10") = a4;
+		register long r8 __asm__("r8") = a5;
+		__asm__ __volatile__("syscall" : "=a"(ret) : "a"(n), "D"(a1), "S"(a2),
+													 "d"(a3), "r"(r10), "r"(r8) : "rcx", "r11", "memory");
+		return ret;
+	}
+
+	static __inline long __syscall6(long n, long a1, long a2, long a3, long a4, long a5, long a6)
+	{
+		unsigned long ret;
+		register long r10 __asm__("r10") = a4;
+		register long r8 __asm__("r8") = a5;
+		register long r9 __asm__("r9") = a6;
+		__asm__ __volatile__("syscall" : "=a"(ret) : "a"(n), "D"(a1), "S"(a2),
+													 "d"(a3), "r"(r10), "r"(r8), "r"(r9) : "rcx", "r11", "memory");
+		return ret;
+	}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ arch/x86/include/syscall_numbers.h
@@ -0,0 +1,7 @@
+#ifndef _SYSCALL_NUMBERS_H_
+#define _SYSCALL_NUMBERS_H_
+
+#define SYS_WRITE 1
+#define SYS_READ 2
+
+#endif
