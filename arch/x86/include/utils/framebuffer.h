81a8c0702f1a9a1beef970b673709b3905887106
artemka <artem.vas333@gmail.com>
2025-07-07 12:10:59 +0300

    merged ramfat

--- arch/x86/include/utils/framebuffer.h
+++ arch/x86/include/utils/framebuffer.h
@@ -10,16 +10,19 @@ typedef struct
     unsigned int pitch;
     unsigned int bpp;
 } framebuffer_info_t;
+
 typedef struct
 {
     void *ramdisk_base;
     uint64_t ramdisk_size;
 } __attribute__((packed)) ramdisk_info_t;
+
 typedef struct
 {
     uint64_t heap_start;
     uint64_t heap_size;
 } ram_info_t;
+
 typedef struct
 {
     framebuffer_info_t *framebuffer;
