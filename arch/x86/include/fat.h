bb022eb705f9c36d5eb4d759d640035a7624eb3e
Svatenko-Stepan <stepenolek@gmail.com>
2025-07-08 21:57:12 +0300

    redesign, delete_file implement

--- arch/x86/include/fat.h
+++ arch/x86/include/fat.h
@@ -4,6 +4,7 @@
 #include "utils/framebuffer.h"
 
 int fat32_init(void *ramdisk_base);
-int fat32_read_file(const char *filename, void *out_buf, size_t *out_size, framebuffer_info_t *fb);
+int fat32_read_file(const char *path, void *out_buf, size_t *out_size);
 int fat32_write_file(const char *filename, const void *data, size_t size);
+int fat32_delete_file(const char *filename);
 void debug_fat32(framebuffer_info_t *fb);
--- arch/x86/kernel/kmain.c
+++ arch/x86/kernel/kmain.c
@@ -24,10 +24,19 @@ void kernel_main(BootInfo *bi)
     size_t sz;
 
     debug_fat32(fb);
-    if (fat32_readgit_file("TEST.TXT", buf, &sz, fb) == 0)
+    if (fat32_read_file("TEXT.TXT", buf, &sz) == 0)
     {
         printf("FILE FOUND\n");
         printf("%s\n", buf);
+        if (fat32_delete_file("TEXT.TXT") == 0)
+        {
+            printf("FILE DELETED\n");
+            if (fat32_read_file("TEXT.TXT", buf, &sz) == 0)
+            {
+                printf("FILE FOUND\n");
+                printf("%s\n", buf);
+            }
+        }
     }
     else
     {
@@ -35,8 +44,6 @@ void kernel_main(BootInfo *bi)
     }
     printf("END\n");
 
-    //     os_main(fb);
-    // return;
     while (1)
         ;
 }
