2ea99bc9540a4e6bdc7e859aee7d228fb77df819
artemka <artem.vas333@gmail.com>
2025-07-11 00:14:21 +0300

    update string.h

--- lib/libc/include/test_stddef.h
+++ lib/libc/include/test_stddef.h
@@ -4,6 +4,10 @@
 
 _Begin_C_Header;
 
+#ifdef __cplusplus
+#define NULL 0L
+#else
 #define NULL ((void *)0)
+#endif
 
 _End_C_Header;
---
+++ lib/libc/src/string/strcoll.c
@@ -0,0 +1,10 @@
+#include <string.h>
+
+// Temporary implementation of strcoll()
+// TODO: Implement locale-aware string comparison in the future.
+// For now, we fall back to simple binary comparison using strcmp.
+
+int strcoll(const char *s1, const char *s2)
+{
+	return strcmp(s1, s2);
+}
