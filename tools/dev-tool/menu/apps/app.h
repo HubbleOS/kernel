0933465c4689fc9888ee95d61edc4f32c897db23
artemka <artem.vas333@gmail.com>
2025-09-10 00:19:28 +0300

    Refactor: wrap checklist and menu in structs, fix build errors

--- tools/dev-tool/menu/apps/app.h
+++ tools/dev-tool/menu/apps/app.h
@@ -1,3 +1,46 @@
 #pragma once
 
+#include <ncurses.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <ui/menu.h>
+#include <ui/main.h>
+
 void app();
+
+// ─────────────────────────────────────────────────────────────────────────────
+// Terminal size warning
+
+void show_terminal_size_warning_window();
+
+// ─────────────────────────────────────────────────────────────────────────────
+// Modal example actions
+
+void on_save();
+void on_dont_save();
+void fun();
+
+void show_save_modal();
+void show_save_modal2();
+
+void start_game();
+
+// ─────────────────────────────────────────────────────────────────────────── ──
+// Menu setup
+
+extern MenuItem main_items[];
+
+typedef struct
+{
+	ChecklistItem *items;
+	size_t count;
+} Checklist;
+
+typedef struct
+{
+	MenuItem *items;
+	size_t count;
+} Menu;
+
+extern Checklist checklists;
+extern Menu main_menu;
---
+++ tools/dev-tool/menu/config/config.c
@@ -0,0 +1,77 @@
+#include <config.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <ui/main.h>
+#include "../apps/app.h"
+
+void config_save(const char *path)
+{
+	FILE *file = fopen(path, "w");
+	if (!file)
+		return;
+
+	for (size_t i = 0; i < checklists.count; i++)
+	{
+		fprintf(file, "%s=%d\n", checklists.items[i].label, checklists.items[i].checked);
+	}
+
+	fclose(file);
+}
+
+void config_init(const char *path)
+{
+	FILE *file = fopen(path, "w");
+	if (!file)
+		return;
+
+	for (size_t i = 0; i < checklists.count; i++)
+	{
+		// fprintf(file, "%s=%d\n", checklists.items[i].label, checklists.items[i].checked);
+		fprintf(file, "%s=%d\n", checklists.items[i].label, 0);
+	}
+
+	fclose(file);
+}
+
+void config_load(const char *path)
+{
+	FILE *file = fopen(path, "r");
+	if (!file)
+	{
+		config.init(path);
+		file = fopen(path, "r");
+		if (!file)
+			return;
+	}
+
+	char line[128];
+	while (fgets(line, sizeof(line), file))
+	{
+		line[strcspn(line, "\n")] = '\0'; // delete \n
+
+		char *key = strtok(line, "=");
+		char *value = strtok(NULL, "=");
+		if (!key || !value)
+			continue;
+
+		for (size_t i = 0; i < checklists.count; i++)
+		{
+			if (strcmp(key, checklists.items[i].label) == 0)
+			{
+				checklists.items[i].checked = atoi(value);
+				break;
+			}
+		}
+	}
+
+	fclose(file);
+}
+
+Config config = {
+    .save = config_save,
+    .init = config_init,
+    .load = config_load,
+};
