e6554b35f0e63fe7950ee8228c9e04552b9aa51a
artemka <artem.vas333@gmail.com>
2025-09-17 22:51:15 +0300

    merged dev/tools

--- tools/dev-tool/qemu/src/qemu.c
+++ tools/dev-tool/qemu/src/qemu.c
@@ -20,8 +20,8 @@ void qemu_build_command(const QemuOptions *opts, char *cmd, size_t len)
 
 	pos += snprintf(cmd + pos, len - pos, "qemu-system-%s ", opts->arch);
 
-	// pos += snprintf(cmd + pos, len - pos, "-M pc ");
-	pos += snprintf(cmd + pos, len - pos, "-M q35 ");
+	pos += snprintf(cmd + pos, len - pos, "-M pc ");
+	// pos += snprintf(cmd + pos, len - pos, "-M q35 ");
 	pos += snprintf(cmd + pos, len - pos, "-cpu Haswell ");
 	pos += snprintf(cmd + pos, len - pos, "-m %d ", opts->mem);
 	pos += snprintf(cmd + pos, len - pos, "-smp %d ", opts->smp);
@@ -30,12 +30,11 @@ void qemu_build_command(const QemuOptions *opts, char *cmd, size_t len)
 	pos += snprintf(cmd + pos, len - pos, "-drive file=%s,format=raw,index=0,media=disk,cache=none ", "../../../out/x86/disk.img");
 
 	// FAT ISO-disk
-	// pos += snprintf(cmd + pos, len - pos, "-drive file=fat:rw:%s,format=raw,index=1,media=disk ", opts->iso_path);
-	pos += snprintf(cmd + pos, len - pos,
-			"-drive file=fat:rw:%s,if=none,id=nvm-1 "
-			"-device nvme,drive=nvm-1,serial=nvme-test ",
-			opts->iso_path);
-	// pos += snprintf(cmd + pos, len - pos, "-drive file=fat:rw:%s,format=raw,index=1,media=disk ", opts->iso_path);
+	// pos += snprintf(cmd + pos, len - pos,
+	// 		"-drive file=fat:rw:%s,if=none,id=nvm-1 "
+	// 		"-device nvme,drive=nvm-1,serial=nvme-test ",
+	// 		opts->iso_path);
+	pos += snprintf(cmd + pos, len - pos, "-drive file=fat:rw:%s,format=raw,index=1,media=disk ", opts->iso_path);
 
 	// OVMF
 	pos += snprintf(cmd + pos, len - pos, "-drive if=pflash,format=raw,readonly=on,file=%s ", ovmf_path);
