2c03252719956cefb78ca18efd090b5a082cd6ae
Svatenko-Stepan <stepenolek@gmail.com>
2026-06-04 08:13:04 +0300

    raspbPI boot implementation

--- arch/arm64/kernel/start.asm
+++ arch/arm64/kernel/start.asm
@@ -1,21 +1,37 @@
-/* AArch64 bare-metal entry point */
-.section .text
-.global _start
+    .section .text.boot
+    .globl _start
+    .type _start, %function
+
 _start:
-    /* Установим стек (16 KB сверху RAM) */
-    ldr x0, =stack_top
+    mrs x0, mpidr_el1
+    and x0, x0, #0xff
+    cbz x0, 1f
+
+0:
+    wfe
+    b 0b
+
+1:
+    ldr x0, =__stack_top
     mov sp, x0
 
-    /* Вызов main() */
-    bl main
+    ldr x0, =__bss_start
+    ldr x1, =__bss_end
+2:
+    cmp x0, x1
+    b.hs 3f
+    str xzr, [x0], #8
+    b 2b
 
-hang:
-    wfe      /* wait for event, просто застыть */
-    b hang
+3:
+    bl kernel_main
 
-/* стек */
-.section .bss
-    .align 16
-stack_bottom:
-    .space 16384
-stack_top:
+4:
+    wfe
+    b 4b
+
+    .section .bss
+    .align 12
+__stack_bottom:
+    .skip 16384
+__stack_top:
